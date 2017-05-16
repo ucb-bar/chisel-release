bf14ca3046507068cfddbdfdbab8502309a2c652
Jim Lawson <ucbjrl@berkeley.edu>
2017-05-16 11:43:51 -0700

    complicate updateSNAPSHOT_version.sh\nSupport default SNAPSHOT suffix as current date.\nSupport explicit version setting.

--- updateSNAPSHOT_version.sh
+++ updateSNAPSHOT_version.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
-releaseDate=$1
+# Set default suffix
+versionSuffix=`date +%Y-%m-%d`
+version=""
+while [[ $# -gt 1 ]]
+do
+opt="$1"
+case $opt in
+  -s|--suffix)
+  versionSuffix="$2"
+  shift
+  ;;
+  -v|--version)
+  version="$2"
+  shift
+  ;;
+  *)
+  print "unknown option: $opt"
+  exit 1
+  ;;
+esac
 shift
-sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)\"/\"\1_$releaseDate\"/" $@
+done
+# If we're replacing the entire version string, limit the replacement
+#  to the actual version setting.
+if [[ $version != "" ]] ; then
+  sed -E -i.bak -e "/\bversion := /s/\"([[:digit:]]+\.[[:digit:]]+(-SNAPSHOT){0,1}(_[^\"]+){0,1}\")/\"$version\"/" $@
+else
+  sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)(_[^\"]+){0,1}\"/\"\1_$versionSuffix\"/" $@
+fi
