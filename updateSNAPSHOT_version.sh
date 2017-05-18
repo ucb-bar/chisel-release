b580c03f0744d4d6d71c6a9d0c32224a75c27f3a
Jim Lawson <ucbjrl@berkeley.edu>
2017-05-18 15:12:48 -0700

    Update  updateSNAPSHOT_version.sh  from master.

--- updateSNAPSHOT_version.sh
+++ updateSNAPSHOT_version.sh
@@ -1,4 +1,58 @@
 #!/bin/sh
-releaseDate=$1
+function usage ()
+{
+  echo usage:
+  cat <<EOF
+$0: [-s suffix | -v version] files...
+
+Search files... for SNAPSHOT version strings, replacing whatever follows the
+SNAPSHOT with the supplied suffix, separated from SNAPSHOT with an '_'.
+If suffix is the empty string (-s ""), any existing suffix is removed.
+NOTE: These replacements will happen whereever a *SNAPSHOT* version is found.
+If a specific version is specified with -v, it will replace the *SNAPSHOT*
+version in its entirety, but this replacement is limited to explicit
+version settings ("version := ...").
+EOF
+}
+
+# Set default suffix
+versionSuffix=`date +%Y-%m-%d`
+version=""
+if [[ $# == 0 ]]; then
+usage
+exit 1
+fi
+while [[ $# -gt 1 || $1 =~ -.* ]]
+do
+opt="$1"
+case $opt in
+  -s|--suffix)
+  if [[ $2 == "" ]]; then
+    versionSuffix=""
+  else
+    versionSuffix=_"$2"
+  fi
+  shift
+  ;;
+  -v|--version)
+  version="$2"
+  shift
+  ;;
+  -h|--help)
+  usage
+  exit 1
+  ;;
+  *)
+  echo "$0: unknown option: $opt"
+  exit 1
+  ;;
+esac
 shift
-sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)\"/\"\1_$releaseDate\"/" $@
+done
+# If we're replacing the entire version string, limit the replacement
+#  to the actual version setting.
+if [[ $version != "" ]] ; then
+  sed -E -i.bak -e "/\bversion := /s/\"([[:digit:]]+\.[[:digit:]]+(-SNAPSHOT){0,1}(_[^\"]+){0,1}\")/\"$version\"/" $@
+else
+  sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)(_[^\"]+){0,1}\"/\"\1$versionSuffix\"/" $@
+fi
