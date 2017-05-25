e139132eb2cd9bc90984701520ea7b2e0b1c624c
Jim Lawson <ucbjrl@berkeley.edu>
2017-05-25 10:27:20 -0700

    Incorporate Pipe internal wiring fix. Cherry-pick fix for chisel3. Update chisel-testers (to use the updated chisel3). Integrate updateSNAPSHOT_version.sh from master (and fix default versionSuffix).

--- updateSNAPSHOT_version.sh
+++ updateSNAPSHOT_version.sh
@@ -1,21 +1,49 @@
 #!/bin/sh
+function usage ()
+{
+  echo usage:
+  cat <<EOF
+$0: [-s suffix | -v version] files...
+
+Search files... for SNAPSHOT version strings, replacing whatever follows the
+SNAPSHOT with the supplied suffix, separated from SNAPSHOT with an '_'.
+If suffix is the empty string (-s ""), any existing suffix is removed.
+NOTE: These replacements will happen whereever a *SNAPSHOT* version is found.
+If a specific version is specified with -v, it will replace the *SNAPSHOT*
+version in its entirety, but this replacement is limited to explicit
+version settings ("version := ...").
+EOF
+}
+
 # Set default suffix
-versionSuffix=`date +%Y-%m-%d`
+versionSuffix=_`date +%Y-%m-%d`
 version=""
-while [[ $# -gt 1 ]]
+if [[ $# == 0 ]]; then
+usage
+exit 1
+fi
+while [[ $# -gt 1 || $1 =~ -.* ]]
 do
 opt="$1"
 case $opt in
   -s|--suffix)
-  versionSuffix="$2"
+  if [[ $2 == "" ]]; then
+    versionSuffix=""
+  else
+    versionSuffix=_"$2"
+  fi
   shift
   ;;
   -v|--version)
   version="$2"
   shift
   ;;
+  -h|--help)
+  usage
+  exit 1
+  ;;
   *)
-  print "unknown option: $opt"
+  echo "$0: unknown option: $opt"
   exit 1
   ;;
 esac
@@ -26,5 +54,5 @@ done
 if [[ $version != "" ]] ; then
   sed -E -i.bak -e "/\bversion := /s/\"([[:digit:]]+\.[[:digit:]]+(-SNAPSHOT){0,1}(_[^\"]+){0,1}\")/\"$version\"/" $@
 else
-  sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)(_[^\"]+){0,1}\"/\"\1_$versionSuffix\"/" $@
+  sed -E -i.bak -e "s/\"([[:digit:]]+\.[[:digit:]]+-SNAPSHOT)(_[^\"]+){0,1}\"/\"\1$versionSuffix\"/" $@
 fi
