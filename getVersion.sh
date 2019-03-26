74484a737c2e49271368c3e113303c85f1f4bda9
Jim Lawson <ucbjrl@berkeley.edu>
2019-03-26 08:21:00 -0700

    Fix getVersion to deal with sbt's frivolous formatting. `sbt` uses an different format to output the version for a multi-project, with no apparent way to obtain just the top level project version. Deal with it.

--- getVersion.sh
+++ getVersion.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # As of 5/3/2017, we include the date in the sbt version so there's no need to add it here.
-sbt version | gawk -e '/^\[info\] [[:digit:]]+\.[[:digit:]]/ { print "v" $2 }'
+# This is very brittle, depending, as it does, on sbt's formatting.
+# We assume that in the case of multiple subprojects, the version is determined
+#  by the root project, whose version is given by the line following:
+#  [info] version
+sbt version | gawk -e 'BEGIN{m=1}' -e '/^\[info\] version$/{m=1}' -e 'm==1 && $1 ~ /^\[info\]/ && $2 ~ /[[:digit:]]+\.[[:digit:]]/ { print "v" $2; m=0 }' -e '/ \/ version$/{m=0}'
