ce41b71d26588f360196fc7ec68f9ac87c2dc640
Jim Lawson <ucbjrl@berkeley.edu>
2019-09-19 08:00:15 -0700

    Make genTag.sh location proof

--- genTag.sh
+++ genTag.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+script_dir=$(dirname $0)
 if [[ $# -gt 0 ]]; then
   version="$1"
 else
-  version=`../getVersion.sh`
+  version=`$script_dir/getVersion.sh`
 fi
 shift
 echo "-m \"master `git rev-parse --short \`git merge-base origin/master HEAD\``\"" "$version"
