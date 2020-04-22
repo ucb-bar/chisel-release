1d8df2847968cf879fb227a7b62f0a9e5b141e26
Jim Lawson <ucbjrl@berkeley.edu>
2020-04-22 09:59:04 -0700

    Merge genTag.sh from master

--- genTag.sh
+++ genTag.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 script_dir=$(dirname $0)
+xbranch="master"
+if [[ $# -gt 0 ]]; then
+  xbranch="$1"
+fi
+shift
 if [[ $# -gt 0 ]]; then
   version="$1"
 else
   version=`$script_dir/getVersion.sh`
 fi
 shift
-echo "-m \"master `git rev-parse --short \`git merge-base origin/master HEAD\``\"" "$version"
+echo "-m \"$xbranch `git rev-parse --short \`git merge-base origin/$xbranch HEAD\``\"" "$version"
